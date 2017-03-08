a79b4140abbeb3849a2c6e8797c4aa019830e023
Jason Robinson <mail@jasonrobinson.me>
2017-03-08 22:45:31 +0200

    Fix texts for the different stacks
    Refs: #43

--- src/texts.js
+++ src/texts.js
@@ -3,21 +3,21 @@ var texts = {
     diaspora: {
       title: 'diaspora*',
       subtitle: 'Share what you want with whom you want',
-      description: 'Open source, decentralized and privacy oriented, diaspora* is the online social world where you are in control',
+      description: 'Open source, decentralized and privacy oriented, diaspora* is the online social world where you are in control.',
       website: 'diasporafoundation.org',
       github: 'https://github.com/diaspora/diaspora'
     },
     friendica: {
-      title: 'friendica',
-      subtitle: 'friendica subtitle',
-      description: 'This is the description of the friendica project',
+      title: 'Friendica',
+      subtitle: 'A Decentralized Social Network',
+      description: 'The Friendica Project is a world-wide consortium of software developers creating decentralised social platforms and technology for the coming post-Facebook world.',
       website: 'friendica.com',
       github: 'https://github.com/friendica/friendica'
     },
     hubzilla: {
-      title: 'hubzilla',
-      subtitle: 'hubzilla subtitle',
-      description: 'This is the description of the hubzilla project',
+      title: 'Hubzilla',
+      subtitle: 'Build a community of connected websites',
+      description: 'Hubzilla is a powerful platform for creating interconnected websites featuring a decentralized identity, communications, and permissions framework built using common webserver technology.',
       website: 'hubzilla.org',
       github: 'https://github.com/redmatrix/hubzilla'
     }
