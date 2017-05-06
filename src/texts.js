4ecae8e02c211572b914e0359bd7800c7a550c2b
Jason Robinson <mail@jasonrobinson.me>
2017-05-06 14:07:01 +0300

    Change Friendica project home page url
    Requested by wilhelm at pirati.ca

--- src/texts.js
+++ src/texts.js
@@ -5,13 +5,15 @@ var texts = {
       subtitle: 'Share what you want with whom you want',
       description: 'Open source, decentralized and privacy oriented, diaspora* is the online social world where you are in control.',
       website: 'diasporafoundation.org',
+      website_protocol: 'https',
       github: 'https://github.com/diaspora/diaspora'
     },
     friendica: {
       title: 'Friendica',
       subtitle: 'A Decentralized Social Network',
       description: 'The Friendica Project is a world-wide consortium of software developers creating decentralised social platforms and technology for the coming post-Facebook world.',
-      website: 'friendica.com',
+      website: 'friendi.ca',
+      website_protocol: 'http',
       github: 'https://github.com/friendica/friendica'
     },
     hubzilla: {
@@ -19,6 +21,7 @@ var texts = {
       subtitle: 'Build a community of connected websites',
       description: 'Hubzilla is a powerful platform for creating interconnected websites featuring a decentralized identity, communications, and permissions framework built using common webserver technology.',
       website: 'hubzilla.org',
+      website_protocol: 'https',
       github: 'https://github.com/redmatrix/hubzilla'
     }
   }
