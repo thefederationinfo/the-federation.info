4462703ab67e3f0efa06a094f7cc00f752a22816
Jason Robinson <mail@jasonrobinson.me>
2018-04-12 22:56:17 +0300

    Start thinking about dockerfiles

--- config/wsgi.py
+++ config/wsgi.py
@@ -2,5 +2,5 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.production")
 application = get_wsgi_application()
