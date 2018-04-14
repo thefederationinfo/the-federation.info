8288716644c866b03d8e80501d1d4a6b515601b2
Jason Robinson <mail@jasonrobinson.me>
2018-04-14 21:58:31 +0300

    Iterating on docker deployment files

---
+++ docker/backend/gunicorn.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+/usr/local/bin/python /app/manage.py collectstatic --noinput
+/usr/local/bin/gunicorn config.wsgi -w 4 -b 0.0.0.0:5000 --chdir=/app -t 300 --log-level debug --capture-output
