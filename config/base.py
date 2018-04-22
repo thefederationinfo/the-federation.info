f85eca6fd5a66ea1fac7c3e4c67c8d0fe400bf51
Jason Robinson <mail@jasonrobinson.me>
2018-04-22 16:41:24 +0300

    Fix app statics on docker deployment

--- config/base.py
+++ config/base.py
@@ -132,7 +132,7 @@ TEMPLATES = [
 STATIC_ROOT = str(ROOT_DIR("staticfiles"))
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-STATIC_URL = "/static/"
+STATIC_URL = "/appstatic/"
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = (
