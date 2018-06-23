d4d71fab1549a9e728f066591e429295b855c976
Jason Robinson <mail@jasonrobinson.me>
2018-06-23 12:11:59 +0300

    Use Platform.icon to find the icon file

--- src/components/NodesTableRow.vue
+++ src/components/NodesTableRow.vue
@@ -63,7 +63,7 @@ export default {
     },
     computed: {
         imageSource() {
-            return `/static/images/${this.node.platform.name}-16.png`
+            return `/static/images/${this.node.platform.icon}-16.png`
         },
         openSignups() {
             return this.node.openSignups ? "Yes" : "No"
