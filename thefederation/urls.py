a091380b4e98eda029e392d5710b5fe8623fdfdc
Jason Robinson <mail@jasonrobinson.me>
2018-08-14 22:56:16 +0300

    Add admin tool to mass register domains

--- thefederation/urls.py
+++ thefederation/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from thefederation.views import register_view
+from thefederation.views import register_view, mass_register_view
 
 urlpatterns = [
+    path("massregister/", mass_register_view, name="massregister"),
     path("register/<host>/", register_view),
 ]
--- thefederation/views.py
+++ thefederation/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import redirect
 
 from thefederation.tasks import poll_node
@@ -14,3 +16,24 @@ def register_view(request, host):
     # TODO show an error or something
     return redirect("/")
 
+
+@staff_member_required
+def mass_register_view(request):
+    input = request.POST.get('domain-list')
+    if not input:
+        return redirect('admin:app_list', app_label="thefederation")
+
+    lines = input.split('\n')
+    lines = [line for line in lines if len(line.strip())]
+    domains = []
+    for line in lines:
+        domains += line.split(',')
+
+    domains = {domain.strip() for domain in domains}
+
+    for domain in domains:
+        poll_node.delay(domain)
+
+    messages.info(request, f"Triggered job to register {len(domains)} domains!")
+
+    return redirect('admin:app_list', app_label='thefederation')
