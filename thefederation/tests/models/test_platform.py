62c84b026ca4311fc2367502a1ba4572ef7da0b2
Jason Robinson <mail@jasonrobinson.me>
2018-12-02 20:26:22 +0200

    Reorganize Platform tests

--- thefederation/tests/models/test_platform.py
+++ thefederation/tests/models/test_platform.py
@@ -4,28 +4,26 @@ from thefederation.models import Platform
 from thefederation.tests.factories import PlatformFactory
 
 
-class GetMethodTestCase(TestCase):
+class PlatformTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         super().setUpTestData()
         cls.platform = PlatformFactory(name='socialhome')
+        cls.hash_platform = PlatformFactory(version_clean_style=Platform.VERSION_CLEAN_REMOVE_COMMIT_HASH)
         cls.unknown_platform = PlatformFactory(name='unknown')
 
-    def test_empty_for_unknown_platform(self):
+    def test_clean_version__remove_commit_hash(self):
+        self.assertEqual(
+            self.hash_platform.clean_version('Pleroma 0.9.0 dca1d6d16278599485df3a175fb356bdc995441c'),
+            'Pleroma 0.9.0',
+        )
+
+    def test_get_method__empty_for_unknown_platform(self):
         self.assertIsNone(self.unknown_platform.get_method((0, 9, 0)))
 
-    def test_returns_a_method(self):
+    def test_get_method__returns_a_method(self):
         self.assertEqual(self.platform.get_method((0, 6, 0)), 'nodeinfo')
         self.assertEqual(self.platform.get_method((0, 9, 0)), 'nodeinfo2')
 
-    def test_survives_empty_version(self):
+    def test_get_method__survives_empty_version(self):
         self.assertIsNone(self.platform.get_method(None))
-
-
-class VersionCleanChoicesTestCase(TestCase):
-    def test_remove_commit_hash(self):
-        platform = PlatformFactory(version_clean_style=Platform.VERSION_CLEAN_REMOVE_COMMIT_HASH)
-        self.assertEqual(
-            platform.clean_version('Pleroma 0.9.0 dca1d6d16278599485df3a175fb356bdc995441c'),
-            'Pleroma 0.9.0',
-        )
