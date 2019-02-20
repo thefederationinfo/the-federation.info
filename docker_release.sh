72c11c2adaee1d133a71730d446ab6852a4567ba
Jason Robinson <mail@jasonrobinson.me>
2019-02-20 23:19:13 +0200

    Simplify docker build for production nginx and release web to hub

--- docker_release.sh
+++ docker_release.sh
@@ -3,5 +3,9 @@
 set -e
 
 docker login
+
 docker build -f docker/app/Dockerfile -t jaywink/the-federation.info .
 docker push jaywink/the-federation.info:latest
+
+docker build -f docker/nginx/Dockerfile -t jaywink/the-federation.info-web .
+docker push jaywink/the-federation.info-web:latest
