ddd4b355052486cc748dc2fcc9e14cad7f8b87d3
Jason Robinson <mail@jasonrobinson.me>
2020-02-15 00:37:40 +0200

    Fix Maxmind GeoIPLite database download
    Needs key set as env variable when downloading :/

--- docker_release.sh
+++ docker_release.sh
@@ -2,9 +2,13 @@
 
 set -e
 
+if [[ -z "${MAXMIND_LICENSE_KEY}" ]]; then
+  echo Define MAXMIND_LICENSE_KEY!
+fi
+
 docker login
 
-docker build -f docker/app/Dockerfile -t jaywink/the-federation.info .
+docker build -f docker/app/Dockerfile -t jaywink/the-federation.info --build-arg "MAXMIND_LICENSE_KEY=${MAXMIND_LICENSE_KEY}" .
 docker push jaywink/the-federation.info:latest
 
 docker build -f docker/nginx/Dockerfile -t jaywink/the-federation.info-web .
