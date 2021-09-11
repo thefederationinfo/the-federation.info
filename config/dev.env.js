b49925021c37c5c7c9dab9e3dcf05202b27168d7
David Sterry <davesterry@gmail.com>
2021-09-10 17:21:54 -0700

    change port to match npm run dev

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,6 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-    API_URI: '"http://127.0.0.1:8000/graphql"',
+    API_URI: '"http://127.0.0.1:8080/graphql"',
     NODE_ENV: '"development"',
 })
