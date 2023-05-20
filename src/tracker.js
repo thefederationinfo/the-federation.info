a2c161186d9e624a234a0e3ef83b11c3a7d7cbc5
crapStone <crapstone@noreply.codeberg.org>
2023-05-20 18:39:55 +0000

    first test of tracker (#309)
    Co-authored-by: crapStone <crapstone01@gmail.com>
    Co-authored-by: 6543 <6543@obermui.de>
    Reviewed-on: https://codeberg.org/thefederationinfo/the-federation.info/pulls/309
    Reviewed-by: Epsilon_02 <epsilon_02@noreply.codeberg.org>
    Reviewed-by: 6543 <6543@obermui.de>

---
+++ src/tracker.js
@@ -0,0 +1,16 @@
+const websiteID = document.getElementById("trker").attributes["data-website-id"].value
+
+const track = path => {
+    try {
+        window.umami.track({
+            website: websiteID,
+            url: path || document.location.pathname,
+            title: document.title,
+            referrer: document.referrer,
+        })
+    } catch (e) {
+        console.warn("could not find umami, ignoring telemetry")
+    }
+}
+
+export default track
