072b202da4f90e6208fd5660598f6c3d03e1eba2
crapStone <crapstone01@gmail.com>
2023-09-05 01:18:29 +0200

    logging doesn't work

--- thefederation/management/commands/recalculate_stats.py
+++ thefederation/management/commands/recalculate_stats.py
@@ -5,8 +5,6 @@ from django.core.management.base import BaseCommand
 
 from thefederation.tasks import aggregate_daily_stats
 
-logger = logging.getLogger(__name__)
-
 
 class Command(BaseCommand):
     help = "Recalculate aggregated daily stats from the given date onward"
@@ -27,10 +25,10 @@ class Command(BaseCommand):
         stop_date = options["until"] or datetime.datetime.now().date()
         current_date = options["start"]
 
-        logger.info(f"recalculating stats from {current_date.isoformat()} to {stop_date.isoformat()}, days: {(stop_date - current_date).days}")
+        self.stdout.write(f"recalculating stats from {current_date.isoformat()} to {stop_date.isoformat()}, days: {(stop_date - current_date).days}")
 
         while current_date <= stop_date:
-            logger.info(f"recalculating stats for {current_date.isoformat()}, remaining days: {(stop_date - current_date).days}")
+            self.stdout.write(f"recalculating stats for {current_date.isoformat()}, remaining days: {(stop_date - current_date).days}")
 
             aggregate_daily_stats(current_date)
 
